5c9c2fa14adb74a4ca3b2016f79a095edb745d6f
Ti Jhae <me@gmail.com>
2020-04-24 15:14:44 +0000

    Updates

---
+++ sw_caches.js
@@ -0,0 +1,44 @@
+const cacheVersion = "version1";
+
+const cacheAssets = ["index.html", "about.html", "css/style.css", "main.js"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches
+      .open(cacheVersion)
+      .then((cache) => {
+        console.log("caching Assets");
+        cache.addAll(cacheAssets);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== cacheVersion) {
+            console.log("deleting old caches");
+            caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      if (res) {
+        console.log("fetching from service worker");
+        return res;
+      } else {
+        console.log("fecthing from live version");
+        fetch(e.request);
+      }
+    })
+  );
+});
